ad31a81cb02fd2434c71e40e02e3d3803eadf1f6
Rafed Fareed <113482156+RafedFareed@users.noreply.github.com>
2022-11-02 13:52:40 -0400

    Add files via upload

---
+++ labweek09/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>101205956</p>
+        <p>Rafed Fareed</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
